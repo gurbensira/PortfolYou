4fd5dd1b5da6e27499015837c123356eba1d6011
gurbensira <gurbs20@gmail.com>
2025-11-11 19:06:43 +0200

    registre form

--- client/src/users/components/RegisterForm.jsx
+++ client/src/users/components/RegisterForm.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { registerUser } from '../services/usersApiService'
 
 function RegisterForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = async (data) => {
+        try {
+            const response = await registerUser(data);
+            console.log('Registration successful:', response);
+            // Handle success
+        } catch (error) {
+            console.error('Registration failed:', error);
+            // Handle error
+        }
+    };
+
     return (
-        <form className='h-[40vh] max-w-md border border-black rounded-md bg-white flex-col flex items-center justify-center gap-5'>
-            <input type="text" placeholder="Username" className='w-[85%] border rounded-md' />
-            <input type="email" placeholder="Email" className='w-[85%] border rounded-md' />
-            <input type="password" placeholder="Password" className='w-[85%] border rounded-md' />
-            <button type="submit" className='w-[85%] py-2 bg-blue-500 text-white rounded-md'>Register</button>
+        <form onSubmit={handleSubmit(onSubmit)}
+            className='h-[40vh] max-w-md border border-black rounded-md bg-white flex-col flex items-center justify-center gap-5'>
+            <input
+                {...register("username", { required: true })}
+                type="text"
+                placeholder="Username"
+                className='w-[85%] border rounded-md'
+            />
+            <input
+                {...register("email", { required: true })}
+                type="email"
+                placeholder="Email"
+                className='w-[85%] border rounded-md'
+            />
+            <input
+                {...register("password", { required: true, minLength: 6 })}
+                type="password"
+                placeholder="Password"
+                className='w-[85%] border rounded-md'
+            />
+            <button type="submit" className='w-[85%] py-2 bg-blue-500 text-white rounded-md'>
+                Submit
+            </button>
         </form>
     )
 }
---
+++ client/src/users/services/usersApiService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+export const registerUser = async (userDetailsForServer) => {
+    try {
+        const response = await axios.post(baseUrl + "/users", userDetailsForServer);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
