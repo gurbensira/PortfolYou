bbf824e64d8f8a1e836913052d1237b1cb324f87
gurbensira <gurbs20@gmail.com>
2026-01-03 15:41:37 +0200

    concurrently install

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: ["http://127.0.0.1:5500", "http://localhost:5173"],
+        origin: ["http://127.0.0.1:5500", "http://localhost:5173", "http://localhost:5174"],
     })
 );
 
